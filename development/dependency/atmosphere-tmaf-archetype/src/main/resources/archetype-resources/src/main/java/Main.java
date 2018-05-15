3a62db65d3338a126e0c3d7a57776b0e3124110d
Nuno Antunes <nmsa@dei.uc.pt>
2018-05-15 02:26:23 +0100

    Added maven archetype (#12)
    Maven Archetype for Java applications.

---
+++ development/dependency/atmosphere-tmaf-archetype/src/main/resources/archetype-resources/src/main/java/Main.java
@@ -0,0 +1,40 @@
+/**
+ * <b>ATMOSPHERE</b> - http://www.atmosphere-eubrazil.eu/
+ * ***
+ * <p>
+ * <b>Trustworthiness Monitoring & Assessment Framework</b>
+ * Component: // FIXME: Define associated component
+ * <p>
+ * Repository: https://github.com/nmsa/tma-framework
+ * License: https://github.com/nmsa/tma-framework/blob/master/LICENSE
+ * <p>
+ * <p>
+ */
+package $package;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Please, add a short description of the class.
+ * <p>
+ *
+ * @author Nuno Antunes <nmsa@dei.uc.pt>
+ */
+public class Main {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
+    /**
+     * Please, document your main methods using <tt>Javadoc</tt>.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        LOGGER.trace("Trust me! This is ATMOSPHERE!");
+        LOGGER.debug("Trust me! This is ATMOSPHERE!");
+        LOGGER.info("Trust me! This is ATMOSPHERE!");
+        LOGGER.warn("Trust me! This is ATMOSPHERE!");
+        LOGGER.error("Trust me! This is ATMOSPHERE!");
+    }
+}
