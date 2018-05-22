90ee784b2abb149fdbf839ba766916aeeac3b1c6
Nuno Antunes <nmsa@dei.uc.pt>
2018-05-22 20:00:56 +0100

    Refactoring code towards issue #2.

--- development/server/monitor-server-python/monitor-api-python/monitor-api.py
+++ development/server/monitor-server-python/monitor-api-python/monitor-api.py
@@ -6,7 +6,6 @@ import logging
 import logging.config
 import os
 
-
 app = Flask(__name__)
 
 logger = logging.getLogger(__name__)
@@ -23,21 +22,21 @@ logger.info('Validator initialized %s', validator)
 
 @app.route('/monitor', methods=['GET', 'POST'])
 def processjson():
-    #reject GET
-    if request.method == 'GET': 
+    # reject GET
+    if request.method == 'GET':
         return "Method GET is not supported"
-    
+
     # load json file
     input = request.get_json(force=True)
     logger.debug('Processing Request %s', input)
-    
-    #todo, validate schema should return number of errors. 
+
+    # todo, validate schema should return number of errors.
     # If 0, proceed with processing
-    return validate_schema (input)
+    return validate_schema(input)
 
 
 def validate_schema(input_msg):
-    #check if there are errors in json file and return the result
+    # check if there are errors in json file and return the result
     errors = [error.message for error in validator.iter_errors(input_msg)]
     if errors:
         response = "Rejected" + "\n" + str(errors) + "\n"
@@ -45,7 +44,8 @@ def validate_schema(input_msg):
     else:
         return "Accepted" + "\n"
 
-# load logging json file
+
+# load logging configuration file
 def setup_logging(default_path='logging.json', env_key='LOG_CFG'):
     path = default_path
     value = os.getenv(env_key, None)
@@ -58,9 +58,9 @@ def setup_logging(default_path='logging.json', env_key='LOG_CFG'):
     else:
         logging.basicConfig(level=logging.DEBUG)
 
+
 if __name__ == '__main__':
     setup_logging()
     logger = logging.getLogger(__name__)
     logger.info('Initializing  Monitor Server Python')
     app.run(debug='True', host='0.0.0.0')
-
