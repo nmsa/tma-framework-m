2920381acbdee04e8faa9912a0b07c0137a6aa71
Nuno Antunes <nmsa@sapo.pt>
2018-05-22 02:35:04 +0100

    Logging added.

--- development/server/monitor-server-python/monitor-api-python/monitor-api.py
+++ development/server/monitor-server-python/monitor-api-python/monitor-api.py
@@ -1,66 +1,62 @@
 from flask import Flask
-from flask import abort
-from flask import jsonify
 from flask import request
-from functools import wraps
 import json
 from jsonschema import Draft4Validator
-from pprint import pprint
-from time import sleep
-from datetime import datetime
-# from schemas.atmosphere_tma_m_schema import m_schema_0_3
-# from schemas.input_todo_schema import input_todo_schema
+import logging
+import logging.config
+import os
 
 
-# todo later get add warning
 app = Flask(__name__)
 
-print "\n\n", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), " - Starting Monitor Server Python\n",
+logger = logging.getLogger(__name__)
+logger.info('Starting Monitor Server Python')
 
 with open('schemas/atmosphere_tma-m_schema.json') as f:
-    message_schema = json.load(f)
+    tma_m_schema = json.load(f)
+logger.debug('Schema loaded %s', tma_m_schema)
 
-validator = Draft4Validator(message_schema)
-
-print datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "message_schema: ", len(message_schema)
-
-print datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "validator: ", validator
+validator = Draft4Validator(tma_m_schema)
+logger.info('Validator initialized %s', validator)
 
 
-
-@app.route('/monitor', methods=['POST'])
+@app.route('/monitor', methods=['GET', 'POST'])
 def processjson():
+    #reject GET
+    if request.method == 'GET': 
+        return "Method GET is not supported"
+    
     input = request.get_json(force=True)
-    print datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "\n##Nuno Here. Why", request.method , "###\n"
-    pprint(input)
+    logger.debug('Processing Request %s', input)
+    
+    #todo, validate schema should return number of errors. 
+    # If 0, proceed with processing
     return validate_schema (input)
 
 
+def validate_schema(input_msg):
+    errors = [error.message for error in validator.iter_errors(input_msg)]
+    if errors:
+        response = "Rejected" + "\n" + str(errors) + "\n"
+        return response
+    else:
+        return "Accepted" + "\n"
 
-
-def validate_schema(input):
-
-    def wrapper(fn):
-        @wraps(fn)
-        def wrapped(*args, ** kwargs):
-            #input = request.get_json(force=True)
-            errors = [error.message for error in validator.iter_errors(input)]
-            if errors:
-                response = "Rejected" + "\n" + str(errors) + "\n"
-                return response
-            else:
-                return "Accepted" + "\n"
-        return wrapped
-    return wrapper 
-
-
-
-
-
+def setup_logging(default_path='logging.json', env_key='LOG_CFG'):
+    path = default_path
+    value = os.getenv(env_key, None)
+    if value:
+        path = value
+    if os.path.exists(path):
+        with open(path, 'rt') as f:
+            config = json.load(f)
+        logging.config.dictConfig(config)
+    else:
+        logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == '__main__':
-
-    print "[MAIN]   Starting Monitor Server Python\n"
-
-    app.run(debug = 'False',host='0.0.0.0')
+    setup_logging()
+    logger = logging.getLogger(__name__)
+    logger.info('Initializing  Monitor Server Python')
+    app.run(debug='True', host='0.0.0.0')
 
