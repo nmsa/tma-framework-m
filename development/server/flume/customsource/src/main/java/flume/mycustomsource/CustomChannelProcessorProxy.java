a1e2382b14b9efb5ed7e24c6d2b70d7e77beee96
Rui Silva <rslv560@gmail.com>
2018-08-21 11:51:32 +0100

    Flume Source Code Update

--- development/server/flume/customsource/src/main/java/flume/mycustomsource/CustomChannelProcessorProxy.java
+++ development/server/flume/customsource/src/main/java/flume/mycustomsource/CustomChannelProcessorProxy.java
@@ -3,14 +3,12 @@ package flume.mycustomsource;
 import org.apache.flume.ChannelSelector;
 import org.apache.flume.Event;
 import org.apache.flume.channel.ChannelProcessor;
-
 import org.apache.flume.event.EventBuilder;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 
 public class CustomChannelProcessorProxy extends ChannelProcessor {
@@ -25,42 +23,38 @@ public class CustomChannelProcessorProxy extends ChannelProcessor {
         m_downstreamChannelProcessor = processor;
     }
     public List<Event> SplitFunction (List<Event> evts){
-        List<Event> aux = new ArrayList<Event>();
+        List<Event> listEvents = new ArrayList<Event>();
         for (Event event : evts){
-            byte[] enventBody = event.getBody();
-            JSONObject testeV = new JSONObject(new String(enventBody));
-            JSONArray data = testeV.getJSONArray("data");
+            byte[] eventBody = event.getBody();
+            JSONObject input = new JSONObject(new String(eventBody));
+            JSONArray data = input.getJSONArray("data");
+            int probeId = input.getInt("probeId");
+            int resourceId = input.getInt("resourceId");
             for (int i=0; i<data.length();i++) {
                 JSONArray observations = data.getJSONObject(i).getJSONArray("observations");
+                String type = data.getJSONObject(i).getString("type");
                 for (int j = 0; j < observations.length(); j++) {
-                    int probeid = testeV.getInt("probeId");
-                    int resourceid = testeV.getInt("resourceId");
-                    String type = data.getJSONObject(i).getString("type");
-                    int descriptionid = data.getJSONObject(i).getInt("descriptionId");
+                    int descriptionId = data.getJSONObject(i).getInt("descriptionId");
                     int time = observations.getJSONObject(j).getInt("time");
-                    Double value = null;
-                    value = observations.getJSONObject(j).getDouble("value");
-                    StringBuilder str = new StringBuilder();
-                    str.append(probeid);
-                    str.append(",");
-                    str.append(resourceid);
-                    str.append(",");
-                    str.append(type);
-                    str.append(",");
-                    str.append(descriptionid);
-                    str.append(",");
-                    str.append(time);
-                    str.append(",");
-                    str.append(value);
-
-                    String message = str.toString();
-                    byte[] s = message.getBytes();
-                    Event aaa = EventBuilder.withBody(s);
-                    aux.add(aaa);
+                    Double value = observations.getJSONObject(j).getDouble("value");
+                    Map<String, String> headers = new HashMap<String, String>();
+                    String probestring;
+                    headers.put("probe", probestring = Integer.toString(probeId));
+                    String resourcestring;
+                    headers.put("resource", resourcestring = Integer.toString(resourceId));
+                    headers.put("type", type);
+                    String descriptionstring;
+                    headers.put("description", descriptionstring = Integer.toString(descriptionId));
+                    String timestring;
+                    headers.put("time", timestring = Integer.toString(time));
+                    String valuestring;
+                    headers.put("value", valuestring = Double.toString(value));
+                    Event observationEvent = EventBuilder.withBody(new byte[0], headers);
+                    listEvents.add(observationEvent);
                 }
             }
         }
-        return aux;
+        return listEvents;
     }
 
     @Override
