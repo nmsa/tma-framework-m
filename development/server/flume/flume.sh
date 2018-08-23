0b2ad9e8e7884503f023052ea3bd816e408713f9
Gyelo <rslv560@gmail.com>
2018-08-23 17:18:12 +0100

    Apache Flume Script Update

--- development/server/flume/flume.sh
+++ development/server/flume/flume.sh
@@ -9,11 +9,11 @@ read escolha
 if [ $escolha -eq 1 ]; then
 
 
-	kubectl exec -ti kafak-0 -- bash -c "cd /flume/apache-flume-1.6.0-bin/bin && ./flume-ng agent --classpath /flume/apache-flume-1.6.0-bin/conf/ -f /flume/apache-flume-1.6.0-bin/conf/flume2.conf -n agent -Dflume.root.logger=INFO,console"
+	kubectl exec -ti kafka-0 -- bash -c "cd /flume/apache-flume-1.6.0-bin/bin && ./flume-ng agent /flume/apache-flume-1.6.0-bin/conf/ -f /flume/apache-flume-1.6.0-bin/conf/flume.conf -n agent -Dflume.root.logger=INFO,console"
 
 elif [ $escolha -eq 2 ]; then
 	
-	kubectl exec -ti kafak-0 -- bash -c "cd /flume/apache-flume-1.6.0-bin/bin && ./flume-ng agent --classpath /flume/apache-flume-1.6.0-bin/conf/ -f /flume/apache-flume-1.6.0-bin/conf/flume1.conf -n agent -Dflume.root.logger=INFO,console"
+	kubectl exec -ti kafka-0 -- bash -c "cd /flume/apache-flume-1.6.0-bin/bin && ./flume-ng agent --classpath /flume/apache-flume-1.6.0-bin/conf/ -f /flume/apache-flume-1.6.0-bin/conf/flume4.conf -n agent -Dflume.root.logger=INFO,console"
 
 elif [ $escolha -eq 3 ]; then
 	break
