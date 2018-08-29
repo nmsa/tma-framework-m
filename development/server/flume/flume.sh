548b15c3694e88efe744a0ace63547ced38f5727
Rui Silva <rslv560@gmail.com>
2018-08-29 11:16:07 +0100

    Flume Files Update

--- development/server/flume/flume.sh
+++ development/server/flume/flume.sh
@@ -1,24 +1,24 @@
-while [[ $escolha -ne 3 ]] ; do 
+while [[ $option -ne 3 ]] ; do 
 
 echo 1. Normal Mode
 echo 2. Testing Mode
 echo 3. Quit
 
-read escolha
+read option
 
-if [ $escolha -eq 1 ]; then
+if [ $option -eq 1 ]; then
 
 
 	kubectl exec -ti flume-0 -- bash -c "cd /flume/apache-flume-1.6.0-bin/bin && ./flume-ng agent /flume/apache-flume-1.6.0-bin/conf/ -f /flume/apache-flume-1.6.0-bin/conf/flume.conf -n agent -Dflume.root.logger=INFO,console"
 
-elif [ $escolha -eq 2 ]; then
+elif [ $option -eq 2 ]; then
 	
 	kubectl exec -ti flume-0 -- bash -c "cd /flume/apache-flume-1.6.0-bin/bin && ./flume-ng agent --classpath /flume/apache-flume-1.6.0-bin/conf/ -f /flume/apache-flume-1.6.0-bin/conf/flume4.conf -n agent -Dflume.root.logger=INFO,console"
 
-elif [ $escolha -eq 3 ]; then
+elif [ $option -eq 3 ]; then
 	break
 else
-	echo Escolha Inválida
+	echo Invalid Option
 fi
 
 done
