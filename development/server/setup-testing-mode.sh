15dd393e67975917c7ae5a4b13b351a08f9ada29
Rui Silva <32508907+rfrsilva@users.noreply.github.com>
2018-08-30 11:20:10 +0100

    Normal Mode and Testing Mode to Apache Flume in a separated Kubernetes Pod (#34)
    * Adding Apache Kafka and Apache Flume File
    * Update Kafka YAML file
    * Apache Flume Testing Mode with Volume

--- development/server/setup-testing-mode.sh
+++ development/server/setup-testing-mode.sh
@@ -16,3 +16,6 @@ kubectl create -f kafka/kafka.yaml
 sleep 20
 
 kubectl exec -ti kafka-0 -- kafka-topics.sh --create --topic topic-monitor --zookeeper zk-0.zk-hs.default.svc.cluster.local:2181 --partitions 1 --replication-factor 1
+kubectl exec -ti kafka-0 -- kafka-topics.sh --create --topic queue-listener --zookeeper zk-0.zk-hs.default.svc.cluster.local:2181 --partitions 1 --replication-factor 1
+
+kubectl create -f flume/flume.yaml
