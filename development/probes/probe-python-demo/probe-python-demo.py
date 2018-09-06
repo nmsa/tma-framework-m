61995e04a2e73171088a5ab12ec7a6ba56000788
Rui Silva <rslv560@gmail.com>
2018-09-06 16:40:58 +0100

    Update Probe to New Data Model

--- development/probes/probe-python-demo/probe-python-demo.py
+++ development/probes/probe-python-demo/probe-python-demo.py
@@ -19,29 +19,28 @@ def send_message(url, message_formated):
 def create_message():
 	# the timestamp is the same for all metrics from this stat variable (Python is not compatible with nanoseconds,
 	#  so [:-4] -> microseconds)
-	timestamp = int(time.time())
 
 	# message to sent to the server API
 	# follow the json schema
 	# sentTime = current time? Or the same timestamp from the metrics?
 	# need to change the probeId, resourceId and messageId
-	message = Message(probeId=0, resourceId=101098, messageId=0, sentTime=int(time.time()), data=None)
-        for i in range(10):
+	message = Message(probeId=1, resourceId=101098, messageId=0, sentTime=int(time.time()), data=None)
+        timestamp = int(time.time())
 	# append measurement data to message
-	 dt = Data(type="measurement", descriptionId=i, observations=None)
-	 obs = Observation(time=timestamp, value=20000.00001 + i)
-	 dt.add_observation(observation=obs)
+	dt = Data(type="measurement", descriptionId=1, observations=None)
+	obs = Observation(time=timestamp, value=20000.00001 + 1)
+	dt.add_observation(observation=obs)
 
 	# append data to message
-	 message.add_data(data=dt)
+	message.add_data(data=dt)
 
 	# append event data to message
-	 dt = Data(type="event", descriptionId=i, observations=None)
-	 obs = Observation(time=timestamp, value=10000.00001 + i)
-	 dt.add_observation(observation=obs)
+	dt = Data(type="event", descriptionId=2, observations=None)
+	obs = Observation(time=timestamp, value=-1)
+	dt.add_observation(observation=obs)
 
 	# append data to message
-	 message.add_data(data=dt)
+	message.add_data(data=dt)
 	# return message formatted in json
 	return json.dumps(message.reprJSON(), cls=ComplexEncoder)
 
@@ -53,4 +52,5 @@ if __name__ == '__main__':
      message_formated = create_message()
      response=communication.send_message(message_formated)
      #response=send_message(url, message_formated)
+     time.sleep(1)
      print (response.text)
