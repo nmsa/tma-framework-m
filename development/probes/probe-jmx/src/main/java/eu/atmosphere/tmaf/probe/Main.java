b08605c171f7f1e5692e2c24dff59a5039d003ad
Rui Silva <rslv560@gmail.com>
2019-09-02 11:58:57 +0100

    Update JMX Probe

--- development/probes/probe-jmx/src/main/java/eu/atmosphere/tmaf/probe/Main.java
+++ development/probes/probe-jmx/src/main/java/eu/atmosphere/tmaf/probe/Main.java
@@ -33,13 +33,19 @@ import eu.atmosphere.tmaf.probe.utils.PropertiesManager;
  * Please, add a short description of the class.
  * <p>
  *
- * @author Nuno Antunes <nmsa@dei.uc.pt>
+ * @author Rui Silva <rfsilva@student.dei.uc.pt>
  */
 public class Main {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     private static final String endpoint = PropertiesManager.getInstance().getProperty("JMX_ENDPOINT");
+
+    private static final String descriptionIdCpu = PropertiesManager.getInstance().getProperty("DESCRIPTION_ID_CPU");
+
+    private static final String descriptionIdMemory  = PropertiesManager.getInstance().getProperty("DESCRIPTION_ID_MEMORY");
+
+    private static final String resourceId  = PropertiesManager.getInstance().getProperty("RESOURCE_ID");
 
     private static int messageId = 0;
 
@@ -49,24 +55,21 @@ public class Main {
      * @param args
      */
     public static void main(String[] args) throws Exception{
-        LOGGER.info("Trust me! This is ATMOSPHERE!");
-//        BackgroundClient client = new BackgroundClient("http://127.0.0.1:5000/monitor");
-       BackgroundClient client = new BackgroundClient();
+      LOGGER.info("Trust me! This is ATMOSPHERE!");
+      BackgroundClient client = new BackgroundClient();
+      client.authenticate(1098, "pass".getBytes());
 
-        client.authenticate(1098, "pass".getBytes());
+      Message message;
 
-        Message message;
-
-        boolean start = client.start();
-        LOGGER.info("start {}!", start);
+      boolean start = client.start();
+      LOGGER.info("start {}!", start);
 
         while (true)
         {
            message = client.createMessage();
-           message.setResourceId(30018);
+           message.setResourceId(Integer.parseInt(resourceId));
 
            JMXServiceURL url = new JMXServiceURL(endpoint);
-
            JMXConnector jmxc = JMXConnectorFactory.connect(url, null);
            jmxc.connect();
 
@@ -79,24 +82,19 @@ public class Main {
            // call the garbage collector before the test using the Memory Mbean
            jmxc.getMBeanServerConnection().invoke(new ObjectName("java.lang:type=Memory"), "gc", null, null);
 
-
-
            //get an instance of the HeapMemoryUsage Mbean
            memoryMbean = jmxc.getMBeanServerConnection().getAttribute(new ObjectName("java.lang:type=Memory"), "HeapMemoryUsage");
            cd = (CompositeData) memoryMbean;
            //get an instance of the OperatingSystem Mbean
            osMbean = jmxc.getMBeanServerConnection().getAttribute(new ObjectName("java.lang:type=OperatingSystem"),"ProcessCpuTime");
 
-
            //get system time and cpu time from last poll
            long cpuAfter = Long.parseLong(osMbean.toString());
-
-
            long cpuDiff = cpuAfter - cpuBefore; //find cpu time between our first and last jmx poll
            cpuBefore = cpuAfter;
 
-           message.addData(new Data(Data.Type.MEASUREMENT, 30082, new Observation(Instant.now().getEpochSecond(), Double.parseDouble(cd.get("used").toString()))));
-           message.addData(new Data(Data.Type.MEASUREMENT, 30083, new Observation(Instant.now().getEpochSecond(), (double)cpuDiff)));
+           message.addData(new Data(Data.Type.MEASUREMENT, Integer.parseInt(descriptionIdMemory), new Observation(Instant.now().getEpochSecond(), Double.parseDouble(cd.get("used").toString()))));
+           message.addData(new Data(Data.Type.MEASUREMENT, Integer.parseInt(descriptionIdCpu), new Observation(Instant.now().getEpochSecond(), (double)cpuDiff)));
 
            client.send(message);
            messageId++;
